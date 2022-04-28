e6fbf256bb5c3620862cfa4610057e4cdb72f711
Valmyr Tavares <valmyrtavares71@gmail.com>
2022-04-27 22:07:03 -0300

    Fazendo o footer e o header

--- app/Controller/Pages/Page.php
+++ app/Controller/Pages/Page.php
@@ -6,11 +6,23 @@ use \App\Utils\View;
 
 class Page
 {
+    private static function getHeader()
+    {
+        return View::render('pages/header');
+    }
+
+    private static function getFooter()
+    {
+        return View::render('pages/footer');
+    }
+
     public static function getPage($title, $content)
     {
         return View::render('pages/page', [
             'title' => $title,
-            'content' => $content
+            'header' => self::getHeader(),
+            'content' => $content,
+            'footer' => self::getFooter()
         ]);
     }
 }

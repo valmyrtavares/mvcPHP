d74cd804af5ed3caf2445a5c318ecb394c7f4c0d
Valmyr Tavares <valmyrtavares71@gmail.com>
2022-04-28 21:24:27 -0300

    Contruindo model

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -3,15 +3,20 @@
 namespace App\Controller\Pages;
 
 use \App\Utils\View;
+use \App\Model\Entity\Organization;
 
 class Home extends Page
 {
     public static function getHome()
     {
+        $obOrganization = new Organization;//É estranho mas tem que estanciar dentro da função
+
+
+        
         $content =  View::render('pages/home', [
-            'name' => 'Valmyr Tavares',
-            'description' => "Canal do youtube: http://youtube.com.br/oficial",
-            'site' => "www.valmyrtavares.com.br"
+            'name' => $obOrganization->name,
+            'description' => $obOrganization->description,
+            'site' => $obOrganization->site
         ]);
 
         return parent::getPage('WDEV', $content);

ba655bc0001711e7227ce2eb1685f18756aafa73
Valmyr Tavares <valmyrtavares71@gmail.com>
2022-05-04 10:51:36 -0300

    implementado os botões de rotas no header e a chamada no index.php

--- app/Utils/View.php
+++ app/Utils/View.php
@@ -3,6 +3,13 @@ namespace App\Utils;
 
 class View
 {
+    private static $vars = [];
+
+    public static function init($vars = [])
+    {
+        self::$vars = $vars;
+    }
+
     private static function getContentView($view)
     {
         $file =  __DIR__. '/../../resources/view/' .$view. '.html';
@@ -15,6 +22,9 @@ class View
     public static function render($view, $vars = [])
     {
         $contentView = self::getContentView($view);
+
+        //MERGE DE VARIÁVEIS DO LAYOUT
+        $vars = array_merge(self::$vars, $vars);
 
         $keys = array_keys($vars);
         $keys = array_map(function ($item) {
--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 require  __DIR__. '/vendor/autoload.php';
 
 use App\Http\Router;
+use \App\Utils\View;
 
 define('URL', 'http://localhost/mvcPHP');
 
+//DEFINE VALOR PADRÃO DAS VARIÁVIS
+View::init([
+    'URL' => URL
+]);
+
+//inicia o roteador
 $obRouter = new Router(URL);
 
 include __DIR__. '\routes\pages.php';
